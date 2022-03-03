064ecf07b5b9a713c9a49787e55bdfe82372d2bd
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-03-03 16:30:18 +0800

    init commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export abstract class Widget {
+  static widgetName: string
+  static image: string
+  static description: string
+  version: string
+  static inlcude: string[]
+  destory: () => void
+}
